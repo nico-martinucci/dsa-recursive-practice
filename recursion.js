cad4b3da236e386bf6dc5edb6bb5513e8a7e417a
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-02-07 14:58:56 -0800

    refactor and optimize to O(n) solutions

--- recursion.js
+++ recursion.js
@@ -1,107 +1,74 @@
 /** product: calculate the product of an array of numbers. */
-
-function product(nums) {
-
-  /**
-   * base case: empty array; return 1
-   * progress: nums[0] * rest of nums
-   *
-   * 1
-   * 4 * p([])
-   * 3 * p([4])
-   * 2 * p([3, 4])
-   * p([2, 3, 4]) => bottom of the stack
-   */
-
-  if (nums.length === 0) return 1;
-  return nums[0] * product(nums.slice(1));
+// O(n) space and time
+function product(nums, i = 0) {
+  // base case
+  if (nums.length === i) return 1;
+  return nums[i] * product(nums, i + 1); // progress
 }
 
 /** longest: return the length of the longest word in an array of words. */
-
-function longest(words) {
-  /**
-   * 0
-   * 4  l([]) ==> Math.max(4, 0) => 4
-   * 2  l(["hola"]) ==> Math.max(2, 4) => 4
-   * 5  l(["hi", "hola"]) ==> Math.max(5, 4) => 5
-   * l(["hello", "hi", "hola"]) => bottom of the stack => 5
-   */
-
-  if (words.length === 0) return 0;
-
-  return Math.max(words[0].length, longest(words.slice(1)));
+// O(n) space and time
+function longest(words, i = 0) {
+  if (words.length === i) return 0;
+  return Math.max(words[i].length, longest(words, i + 1));
 }
 
 /** everyOther: return a string with every other letter. */
-
-function everyOther(str) {
-
-  /**
-   * ""
-   * "o" e("")
-   * "l" e("o") ?
-   * "h" e("llo")
-   * e("hello")
-   */
-
-  if (str.length === 0) return "";
-
-  return str[0] + everyOther(str.slice(2));
+// O(n) space and time
+function everyOther(str, i = 0) {
+  if (str.length <= i) return "";
+  return str[i] + everyOther(str, i + 2);
 }
 
 /** find: return boolean depending on if val exists in array or not. */
-
-function find(arr, val) {
-  if (arr.length === 0) return false;
-
-  return arr[0] === val || find(arr.slice(1), val);
+// O(n) space and time
+function find(arr, val, i = 0) {
+  if (arr.length === i) return false;
+  return arr[i] === val || find(arr, val, i + 1);
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
+// O(n) space and time
+function isPalindrome(str, left = 0, right = str.length - 1) {
+  // base case
+  if (left >= right) return true;
 
-function isPalindrome(str) {
-  if (str.length <= 1) return true;
-
-  return str[0] === str[str.length - 1] && isPalindrome(str.slice(1, str.length - 1));
+  if (str[left] === str[right]) {
+    return isPalindrome(str, left + 1, right - 1); // progress
+  } else {
+    return false;
+  }
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
-  if (str.length <= 1) return str;
-
-  return str[str.length - 1] + revString(str.slice(0, str.length - 1));
+function revString(str, i = str.length - 1) {
+  // base case
+  if (i < 0) return "";
+  return str[i] + revString(str, i - 1); // progress
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val, i = 0) {
+  // base case
   if (i >= arr.length) return -1;
-
-  return arr[i] === val ? i : findIndex(arr, val, i + 1);
+  return arr[i] === val ? i : findIndex(arr, val, i + 1); // progress
 
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
-
-  let results = [];
-
-  function _gatherStrs(obj) {
-    for (let val of Object.values(obj)) {
-      if (typeof val === "object") {
-        _gatherStrs(val);
-      } else if (typeof val === "string") {
-        results.push(val);
-      }
+function gatherStrings(obj, results = []) {
+  for (let val of Object.values(obj)) {
+    if (typeof val === "object") {
+      gatherStrings(val, results);
+    } else if (typeof val === "string") {
+      results.push(val);
     }
   }
 
-  _gatherStrs(obj);
   return results;
-
 }
 
 // FURTHER STUDY
